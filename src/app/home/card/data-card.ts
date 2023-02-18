3628dc8113d175d26ef3d36ebc66e9cbc3b305c6
Rosario Nici <rosarionici95@gmail.com>
2023-02-18 20:02:55 +0100

    link

--- src/app/home/card/data-card.ts
+++ src/app/home/card/data-card.ts
@@ -5,7 +5,7 @@ export const dataCard = [
     img:'../../../assets/img/consulence.jpg',
     descrizione:'SEQA guida i sui clienti a raggiungere standard qualitativi elevati. Abbracciando qualsiasi aspetto della gestione del comparto. Insieme possiamo garantire un approccio sistematico alle soluzioni. Ci occuperemo di mettere in regola e supportarti durante tutti i processi legati alla sicurezza alimentare',
     icon: 'fa-solid fa-handshake',
-    link:'#'
+    link:'consulenza'
   },
   {
     title:'Formazione',
@@ -13,7 +13,7 @@ export const dataCard = [
     img:'../../../assets/img/form.jpg',
     descrizione:'In ottemperanza alle normative vigenti in materia di qualità e sicurezza agroalimentare, in particolare per il Regolamento (CE) n. 852/2004, tutti gli operatori del settore alimentare che entrino in contatto diretto o indiretto con alimenti e/o bevande, devono necessariamente seguire dei corsi di formazione che diano delle nozioni di base sulle procedure igienico-sanitarie da adottare.',
     icon: 'fa-solid fa-graduation-cap',
-    link:'#'
+    link:'formazione'
   },
   {
     title:'Avvio Attività',
@@ -21,7 +21,7 @@ export const dataCard = [
     img:'../../../assets/img/avvio.jpg',
     descrizione:'Oltre gli aspetti tecnici, SEQA si fa carico di guidare le realta aziendali nell approccio ai finanziamenti pubblici per il settore Agroalimentare, quaindi aiutare le aziende di tutto il settore ad accedere ai bandi indetti dagli enti preposti all erogazione di finanziamenti pubblici anche a Fondo Perduto',
     icon: 'fa-solid fa-scissors',
-    link:'#'
+    link:'avvio'
   },
   {
     title:'Certificazioni',
@@ -29,7 +29,7 @@ export const dataCard = [
     img:'../../../assets/img/certify.webp',
     descrizione:'Le certificazioni sono fondamentali per rispondere alle richieste sempre più insistenti del mercato in termini di: qualità, salute, ambiente, sicurezza alimentare. Elemento di differenziazione sul mercato, che da una plus alle aziende rispetto ai competitor. Inolte si hanno numero opportunità di approdare in nuovi mercati.',
     icon: 'fa-solid fa-certificate',
-    link:'#'
+    link:'certificazioni'
   },
    {
     title:'Audit',
@@ -37,7 +37,7 @@ export const dataCard = [
     img:'../../../assets/img/audit.jpg',
     descrizione:'All’interno del mondo dei sistemi di gestione certificabili, l Audit è il documento di riferimento per l esecuzione di questi. SEQA dispone per i suoi clienti servizi di audit di prima e seconda parte. Fondamentali per qualifica foritore, collaudo sistemi di gestione, controllo applicazione standard.',
     icon: 'fa-solid fa-magnifying-glass',
-    link:'#'
+    link:'audit'
   },
   {
    title:'Analisi',
@@ -45,7 +45,7 @@ export const dataCard = [
     img:'../../../assets/img/audit.jpg',
     descrizione:'Al fine di garantire il rispetto ai requisiti di igiene, in ottemperanza ai Regolamenti (CE) n. 178/2002 e n. 852/2004, in tutte le fasi della produzione, trasformazione e distribuzione, le aziende operanti nel settore agroalimentare devono mettere in atto delle misure di prevenzione in regime di autocontrollo, tra queste rientrano quelle di campionamento e analisi.',
     icon: 'fa-sharp fa-solid fa-microscope',
-    link:'#'
+    link:'analisi'
 
   }
 ]
