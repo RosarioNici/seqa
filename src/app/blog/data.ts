0240d34a7e315551d5883c7832d6250dca59f12f
Rosario Nici <rosarionici95@gmail.com>
2023-04-19 17:34:17 +0200

    end for today

--- src/app/blog/data.ts
+++ src/app/blog/data.ts
@@ -2,7 +2,7 @@ export const appart = [
   {
     title: 'Le norme ISO',
     hast: 'seqa_official',
-    image: '../../assets/img/iso-logo.png',
+    image: '../../assets/img/images.png',
     description: 'Le normative ISO sono un insieme di standard internazionali sviluppati dallOrganizzazione Internazionale per..',
     position:'Dott. Califano',
     link:'iso'
