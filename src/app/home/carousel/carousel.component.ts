180121b12c04b70512dba5279c8eac5e31ce74d6
Rosario Nici <rosarionici95@gmail.com>
2023-05-03 14:49:57 +0200

    cookie

--- src/app/home/carousel/carousel.component.ts
+++ src/app/home/carousel/carousel.component.ts
@@ -1,9 +1,16 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss']
 })
-export class CarouselComponent {
+export class CarouselComponent implements AfterViewInit {
+  @ViewChild('myVideo') myVideo: any;
+
+  ngAfterViewInit() {
+    this.myVideo.nativeElement.play();
+  }
 }
+
+
